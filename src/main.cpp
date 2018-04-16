c335f9047a67e6b05e83b573adf5fd0aece21d47
Nick Playfair <nick@chaosemerald.co.uk>
2018-04-16 12:40:13 +0100

    Establish connection to RFID reader and use example code to dump info

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,31 @@
 #include <SPI.h>
 #include <MFRC522.h>
 
+#define RST_PIN         6          // Reset pin connected to D6
+#define SS_PIN          10         // SDA pin connected to D10
+
+MFRC522 mfrc522(SS_PIN, RST_PIN);  // Create MFRC522 instance
+
 void setup() {
-    // put your setup code here, to run once:
+
+	Serial.begin(9600);		// Initialize serial communications with the PC
+	SPI.begin();			// Init SPI bus
+	mfrc522.PCD_Init();		// Init MFRC522
+	mfrc522.PCD_DumpVersionToSerial();	// Show details of PCD - MFRC522 Card Reader details
+	Serial.println(F("Scan PICC to see UID, SAK, type, and data blocks..."));
 }
 
 void loop() {
-    // put your main code here, to run repeatedly:
+    // Look for new cards
+	if ( ! mfrc522.PICC_IsNewCardPresent()) {
+		return;
+	}
+
+	// Select one of the cards
+	if ( ! mfrc522.PICC_ReadCardSerial()) {
+		return;
+	}
+
+	// Dump debug info about the card; PICC_HaltA() is automatically called
+	mfrc522.PICC_DumpToSerial(&(mfrc522.uid));
 }
